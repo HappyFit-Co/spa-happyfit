eb9ccd2e19ab990ad4f0bdd5dc76f5edd48b3018
phTononFerreira <pehetofe@gmail.com>
2023-06-12 16:55:28 -0300

    🔨 Correção de bugs

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,29 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 // import './shared/forms/TraducoesYup';
-import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 import { AppRoutes } from './routes';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 // import { AuthProvider } from '';
 
-import Dashboard from './pages/Dashboard';
 
 export const App = () => {
   return (
-    // <AuthProvider>
     <AppThemeProvider>
-
-
       <DrawerProvider>
-        {/* para o react router dom fazer as rotas funcionarem */}
-        <BrowserRouter> 
-
-            <Dashboard/>
-            <AppRoutes />
-
+        <BrowserRouter>
+          <AppRoutes />
         </BrowserRouter>
       </DrawerProvider>
-
-
     </AppThemeProvider>
-    /* </AuthProvider> */ 
   );
 };
