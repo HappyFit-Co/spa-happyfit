2672051ccd1fd632d563061a86893396732b04f3
Tchuc01 <thuco.jandinopolis@gmail.com>
2023-06-17 21:50:32 -0300

    feat: adiciona seleção de sexo e nivel de atividade

--- spa/src/pages/Registro.tsx
+++ spa/src/pages/Registro.tsx
@@ -1,5 +1,5 @@
 import { CakeOutlined, Lock, Mail, MonitorWeightOutlined, Person, StraightenOutlined } from '@mui/icons-material';
-import { Avatar, Box, Button, Grid, InputAdornment, Link, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, FormControl, FormControlLabel, FormLabel, Grid, InputAdornment, InputLabel, Link, MenuItem, Radio, RadioGroup, Select, Slider, TextField, Typography } from '@mui/material';
 import backgroundImage from '../assets/images/fundo_academia.png';
 import logoImage from '../assets/images/logo_happy.png';
 
@@ -29,7 +29,7 @@ const Registro = () => {
   useEffect(() => {
     document.title = TITULO_PAGINA
   }, []);
-  
+
   const [error, setError] = useState('')
   const [etapa, setEtapa] = useState(1)
 
@@ -41,6 +41,13 @@ const Registro = () => {
   const [birthday, setBirthday] = useState('');
   const [sex, setSex] = useState('');
   const [activityLevel, setActivityLevel] = useState('');
+
+  const [sliderValue, setSliderValue] = useState(2); // 0: Baixo, 1: Médio, 2: Alto
+  const sliderLabels = ['Baixo', 'Médio', 'Alto'];
+
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
+  };
 
   const handleNomeChange = (event) => {
     setNome(event.target.value);
@@ -85,12 +92,12 @@ const Registro = () => {
       setError("Email inválido")
       return
     }
-  
+
     if (pwd.length < 8 || !/[a-zA-Z]/.test(pwd)) {
       setError("Senha inválida. A senha deve ter pelo menos 8 caracteres e conter pelo menos uma letra.")
       return
     }
-  
+
     setEtapa(2)
     setError('')
   }
@@ -108,7 +115,6 @@ const Registro = () => {
     }
     console.log({ nome, email, pwd, weight, height, birthday, sex, activityLevel });
   }
-
   return (
     etapa === 1 ? (
       // ETAPA 1
@@ -145,6 +151,17 @@ const Registro = () => {
                 }}
               />
               <Typography variant="h1" color='#666666' fontSize='4vh' fontWeight='600' paddingTop="2vh">Cadastrar</Typography>
+              <FormControl sx={{ paddingTop:"3vh"}}>
+                <FormLabel>Sexo</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="female"
+                >
+                  <FormControlLabel value="female" control={<Radio />} label="Feminino" />
+                  <FormControlLabel value="male" control={<Radio />} label="Masculino" />
+                </RadioGroup>
+              </FormControl>
               <TextField
                 id="nome-field"
                 label="Nome"
@@ -255,11 +272,11 @@ const Registro = () => {
                   alignSelf: 'center'
                 }}
               />
-              <Typography variant="h1" color='#666666' fontSize='3vh' fontWeight='600' paddingTop="2vh">Informações</Typography>
+              <Typography variant="h1" color='#666666' fontSize='3vh' fontWeight='600' paddingTop="4vh">Informações</Typography>
               <TextField
                 id="birthday-field"
                 key="birthday"
-                label="Data de Aniversário"
+                label="Data de Nascimento"
                 variant="outlined"
                 margin='normal'
                 type='required'
@@ -314,8 +331,26 @@ const Registro = () => {
                   width: '100%',
                 }}
               />
+              <Typography paddingTop="1vh" variant="body1" color="textSecondary">
+                Nível de atividade:
+              </Typography>
+              <Slider
+                value={sliderValue}
+                onChange={handleSliderChange}
+                min={0}
+                max={2}
+                step={1}
+                sx={{
+                  width: '90%',
+                  alignSelf: 'center'
+                }}
+                marks={sliderLabels.map((label, index) => ({
+                  value: index,
+                  label: label,
+                }))}
+              />
               <Typography width="18vw" textAlign="center" color='red'>{error}</Typography>
-              <Box display="flex" flexDirection="row" gap="1vh">
+              <Box paddingTop="3vh" display="flex" flexDirection="row" gap="1vh">
                 <Button onClick={handleBackStep} variant="contained" sx={{ borderRadius: '20vh', height: '5vh', margin: '2vh 0 0 0', width: '100%', '&:hover': { color: 'white' } }}>Voltar</Button>
                 <Button onClick={handleRegisterUser} variant="contained" sx={{ borderRadius: '20vh', height: '5vh', margin: '2vh 0 0 0', width: '100%', '&:hover': { color: 'white' } }}>Finalizar</Button>
               </Box>
